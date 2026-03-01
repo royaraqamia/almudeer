b440f3d50f0cf6857faefcca0fc6830271d6ca8f
Ayham Alali <i.ayhamalali@gmail.com>
2026-03-01 13:47:49 +0300

    Update!

--- services/__init__.py
+++ services/__init__.py
@@ -5,18 +5,16 @@ from .telegram_service import TelegramService, TelegramBotManager, TELEGRAM_SETU
 from .gmail_oauth_service import GmailOAuthService
 from .gmail_api_service import GmailAPIService
 from .telegram_phone_service import TelegramPhoneService, get_telegram_phone_service
-from .agora_service import AgoraService
 
 __all__ = [
     'EmailService',
     'EMAIL_PROVIDERS',
-    'TelegramService', 
+    'TelegramService',
     'TelegramBotManager',
     'TELEGRAM_SETUP_GUIDE',
     'GmailOAuthService',
     'GmailAPIService',
     'TelegramPhoneService',
     'get_telegram_phone_service',
-    'AgoraService',
 ]
 
